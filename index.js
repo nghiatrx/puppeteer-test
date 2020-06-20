c7b8864eb23ed1dc21a77877e7bb387f9386e6c0
Dao Trong Nghia <nghia@blueberrylabs.com>
2020-06-20 15:50:44 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ fastify.get("/", async (request, reply) => {
 });
 
 // Run the server!
-fastify.listen(3000, (err, address) => {
+fastify.listen(3000, "0.0.0.0", (err, address) => {
   if (err) throw err;
   fastify.log.info(`server listening on ${address}`);
 });
